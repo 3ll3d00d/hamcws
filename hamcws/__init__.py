7540b0d35776f8ca189776adc0b28994b9c46173
3ll3d00d <mattkhan@gmail.com>
2024-01-01 17:44:22 +0000

    expose the connection class

--- hamcws/__init__.py
+++ hamcws/__init__.py
@@ -1,1 +1,1 @@
-from .hamcws import get_mcws_connection, MediaServer, Zone, CannotConnectError, InvalidAuthError
+from .hamcws import get_mcws_connection, MediaServer, MediaServerConnection, Zone, CannotConnectError, InvalidAuthError
