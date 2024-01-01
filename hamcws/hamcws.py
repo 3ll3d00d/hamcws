049227facd68e8520ab02889aa0366a178eeaee6
3ll3d00d <mattkhan@gmail.com>
2024-01-01 15:29:56 +0000

    implement library field lookups

--- hamcws/hamcws.py
+++ hamcws/hamcws.py
@@ -1,5 +1,5 @@
 """Implementation of a MCWS inteface."""
-from typing import Tuple, List
+from typing import Tuple, List, Callable, TypeVar, Union
 from xml.etree import ElementTree as et
 
 from aiohttp import ClientSession, ClientResponseError, BasicAuth
@@ -40,6 +40,9 @@ class Zone:
         return self.name
 
 
+T = TypeVar("T", bound=Union[list, dict])
+
+
 def get_mcws_connection(host: str, port: int, username: str | None = None, password: str | None = None,
                         ssl: bool = False, timeout: int = 5, session: ClientSession = None):
     """Returns a MCWS connection."""
@@ -61,12 +64,19 @@ class MediaServerConnection:
         self._auth = BasicAuth(username, password) if username is not None else None
         self._base_url = f"http{'s' if ssl else ''}://{host}:{port}/MCWS/v1"
 
-    async def get(self, path: str, params: dict | None = None) -> Tuple[bool, dict]:
+    async def get_as_dict(self, path: str, params: dict | None = None) -> Tuple[bool, dict]:
+        return await self.__get(path, _to_dict, params)
+
+    async def get_as_list(self, path: str, params: dict | None = None) -> Tuple[bool, list]:
+        return await self.__get(path, _to_list, params)
+
+    async def __get(self, path: str, parser: Callable[[str], Tuple[bool, T]],
+                    params: dict | None = None) -> Tuple[bool, T]:
         async with self._session.get(f'{self._base_url}/{path}', params=params, timeout=self._timeout,
                                      auth=self._auth) as resp:
             try:
                 resp.raise_for_status()
-                return self.__to_dict(await resp.text())
+                return parser(await resp.text())
             except ClientResponseError as e:
                 if e.status == 401:
                     raise InvalidAuthError from e
@@ -80,24 +90,36 @@ class MediaServerConnection:
             self._session = None
             self._close_session_on_exit = False
 
-    @staticmethod
-    def __to_dict(content: str, try_int_cast: bool = False) -> Tuple[bool, dict]:
-        """
-        Converts the relatively unstructured XML responses from MCWS into a dictionary with a flag to indicate
-        if the response was "OK".
-        """
-        result: dict = {}
-        root = et.fromstring(content)
-        for child in root:
-            result[child.attrib["Name"]] = child.text
-        if try_int_cast:
-            for key in result:
-                if key:
-                    try:
-                        result[key] = int(result[key])
-                    except ValueError:
-                        pass  # passing is ok: If untranslatable, leave alone
-        return root.attrib['Status'] == 'OK', result
+
+def _to_dict(content: str) -> Tuple[bool, dict]:
+    """
+    Converts the MCWS XML response into a dictionary with a flag to indicate if the response was "OK".
+    Used where the child Item elements represent different fields (aka have the Name attribute) providing data about a
+    single entity.
+    """
+    result: dict = {}
+    root = et.fromstring(content)
+    for child in root:
+        result[child.attrib["Name"]] = child.text
+    return root.attrib['Status'] == 'OK', result
+
+
+def _to_list(content: str) -> Tuple[bool, list]:
+    """
+    Converts the MCWS XML response into a list of values with a flag to indicate if the response was "OK".
+    Used where the child Item elements have no name attribute and are just providing a list of distinct string values
+    which are typically values from the same library field..
+    """
+    result: list = []
+    root = et.fromstring(content)
+    for child in root:
+        result.append(child.text)
+    return root.attrib['Status'] == 'OK', result
+
+
+AUDIO_FILTER = '[Media Type]=[Audio]'
+MOVIE_FILTER = '[Media Type]=[Video] [Media Sub Type]=[Movie]'
+TV_FILTER = '[Media Type]=[Video] [Media Sub Type]=[TV Show]'
 
 
 class MediaServer:
@@ -111,12 +133,12 @@ class MediaServer:
 
     async def can_connect(self) -> bool:
         """ returns true if server allows the connection. """
-        ok, resp = await self._conn.get('Authenticate')
+        ok, resp = await self._conn.get_as_dict('Authenticate')
         return 'Token' in resp.keys()
 
     async def get_zones(self) -> List[Zone]:
         """ all known zones """
-        ok, resp = await self._conn.get("Playback/Zones")
+        ok, resp = await self._conn.get_as_dict("Playback/Zones")
         num_zones = int(resp["NumberZones"])
         active_zone_id = resp['CurrentZoneID']
         return [Zone(resp, i, active_zone_id) for i in range(num_zones)]
@@ -125,7 +147,7 @@ class MediaServer:
         params = self.__zone_params(zone)
         if extra_fields:
             params['Fields'] = ';'.join(extra_fields)
-        ok, resp = await self._conn.get("Playback/Info")
+        ok, resp = await self._conn.get_as_dict("Playback/Info")
         if 'Playback Info' in extra_fields:
             pass  # parse into a nested dict
         return resp
@@ -136,12 +158,14 @@ class MediaServer:
 
     async def volume_up(self, step: float = 0.1, zone: Zone | None = None) -> None:
         """Send volume up command."""
-        await self._conn.get('Playback/Volume', params={'Level': step, 'Relative': 1, **self.__zone_params(zone)})
+        await self._conn.get_as_dict('Playback/Volume',
+                                     params={'Level': step, 'Relative': 1, **self.__zone_params(zone)})
 
     async def volume_down(self, step: float = 0.1, zone: Zone | None = None) -> None:
         """Send volume down command."""
-        await self._conn.get('Playback/Volume', params={'Level': f'{"-" if step > 0 else ""}{step}', 'Relative': 1,
-                                                        **self.__zone_params(zone)})
+        await self._conn.get_as_dict('Playback/Volume',
+                                     params={'Level': f'{"-" if step > 0 else ""}{step}', 'Relative': 1,
+                                             **self.__zone_params(zone)})
 
     async def set_volume_level(self, volume: float, zone: Zone | None = None) -> None:
         """Set volume level, range 0-100."""
@@ -150,56 +174,57 @@ class MediaServer:
         if volume > 100:
             raise ValueError(f'{volume} not in range 0-100')
         volume = volume / 100
-        await self._conn.get('Playback/Volume', params={'Level': volume, **self.__zone_params(zone)})
+        await self._conn.get_as_dict('Playback/Volume', params={'Level': volume, **self.__zone_params(zone)})
 
     async def mute(self, mute: bool, zone: Zone | None = None) -> None:
         """Send (un)mute command."""
-        await self._conn.get('Playback/Mute', params={'Set': '1' if mute else '0', **self.__zone_params(zone)})
+        await self._conn.get_as_dict('Playback/Mute', params={'Set': '1' if mute else '0', **self.__zone_params(zone)})
 
     async def play_pause(self, zone: Zone | None = None) -> None:
         """Send play/pause command."""
-        await self._conn.get('Playback/PlayPause', params=self.__zone_params(zone))
+        await self._conn.get_as_dict('Playback/PlayPause', params=self.__zone_params(zone))
 
     async def play(self, zone: Zone | None = None) -> None:
         """Send play command."""
-        await self._conn.get('Playback/Play', params=self.__zone_params(zone))
+        await self._conn.get_as_dict('Playback/Play', params=self.__zone_params(zone))
 
     async def pause(self, zone: Zone | None = None) -> None:
         """Send pause command."""
-        await self._conn.get('Playback/Pause', params=self.__zone_params(zone))
+        await self._conn.get_as_dict('Playback/Pause', params=self.__zone_params(zone))
 
     async def stop(self, zone: Zone | None = None) -> None:
         """Send stop command."""
-        await self._conn.get('Playback/Stop', params=self.__zone_params(zone))
+        await self._conn.get_as_dict('Playback/Stop', params=self.__zone_params(zone))
 
     async def next_track(self, zone: Zone | None = None) -> None:
         """Send next track command."""
-        await self._conn.get('Playback/Next', params=self.__zone_params(zone))
+        await self._conn.get_as_dict('Playback/Next', params=self.__zone_params(zone))
 
     async def previous_track(self, zone: Zone | None = None) -> None:
         """Send previous track command."""
         # TODO does it go to the start of the current track?
-        await self._conn.get('Playback/Previous', params=self.__zone_params(zone))
+        await self._conn.get_as_dict('Playback/Previous', params=self.__zone_params(zone))
 
     async def media_seek(self, position: float, zone: Zone | None = None) -> None:
         """seek to a specified position in ms."""
-        await self._conn.get('Playback/Position', params={'Position': position, **self.__zone_params(zone)})
+        await self._conn.get_as_dict('Playback/Position', params={'Position': position, **self.__zone_params(zone)})
 
     async def play_item(self, item: str, zone: Zone | None = None) -> None:
-        await self._conn.get('Playback/PlayByKey', params={'Key': item, **self.__zone_params(zone)})
+        await self._conn.get_as_dict('Playback/PlayByKey', params={'Key': item, **self.__zone_params(zone)})
 
     async def play_playlist(self, playlist_id: str, zone: Zone | None = None) -> None:
         """Play the given playlist."""
-        await self._conn.get('Playback/PlayPlaylist', params={'Playlist': playlist_id, **self.__zone_params(zone)})
+        await self._conn.get_as_dict('Playback/PlayPlaylist',
+                                     params={'Playlist': playlist_id, **self.__zone_params(zone)})
 
     async def play_file(self, file: str, zone: Zone | None = None) -> None:
         """Play the given file."""
-        await self._conn.get('Playback/PlayByFilename', params={'Filenames': file, **self.__zone_params(zone)})
+        await self._conn.get_as_dict('Playback/PlayByFilename', params={'Filenames': file, **self.__zone_params(zone)})
 
     async def set_shuffle(self, shuffle: bool, zone: Zone | None = None) -> None:
         """Set shuffle mode, for the first player."""
-        await self._conn.get('Control/MCC', params={'Command': '10005', 'Parameter': '4' if shuffle else '3',
-                                                    **self.__zone_params(zone)})
+        await self._conn.get_as_dict('Control/MCC', params={'Command': '10005', 'Parameter': '4' if shuffle else '3',
+                                                            **self.__zone_params(zone)})
 
     async def _add_item_to_playlist(self, item):
         # await self._server.Playlist.Add(**{"playlistid": 0, "item": item})
@@ -222,98 +247,94 @@ class MediaServer:
 
     async def clear_playlist(self, zone: Zone | None = None):
         """Clear default playlist."""
-        await self._conn.get('Playback/ClearPlaylist', params=self.__zone_params(zone))
+        await self._conn.get_as_dict('Playback/ClearPlaylist', params=self.__zone_params(zone))
 
-    async def get_artists(self, properties=None):
+    async def get_artists(self) -> list:
         """Get artists list."""
-        # return await self._server.AudioLibrary.GetArtists(
-        #     **_build_query(properties=properties)
-        # )
-        raise NotImplementedError
+        ok, resp = await self._conn.get_as_list('Library/Values',
+                                                params={'Field': 'Artist', 'Files': AUDIO_FILTER})
+        return resp
 
-    async def get_artist_details(self, artist_id=None, properties=None):
+    async def get_artist_details(self, artist_id=None):
         """Get artist details."""
         # return await self._server.AudioLibrary.GetArtistDetails(
         #     **_build_query(artistid=artist_id, properties=properties)
         # )
         raise NotImplementedError
 
-    async def get_albums(self, artist_id=None, album_id=None, properties=None):
+    async def get_albums(self, artist_name=None, album_name=None):
         """Get albums list."""
-        # filter = {}
-        # if artist_id:
-        #     filter["artistid"] = artist_id
-        # if album_id:
-        #     filter["albumid"] = album_id
-        #
-        # return await self._server.AudioLibrary.GetAlbums(
-        #     **_build_query(filter=filter, properties=properties)
-        # )
-        raise NotImplementedError
+        item_filter = AUDIO_FILTER
+        if artist_name:
+            item_filter = f'{item_filter} [Artist]=[{artist_name}'
+        if album_name:
+            item_filter = f'{item_filter} [Album]=[{album_name}'
+        ok, resp = await self._conn.get_as_list('Library/Values', params={'Field': 'Album', 'Files': item_filter})
+        return resp
 
-    async def get_album_details(self, album_id, properties=None):
+    async def get_album_details(self, album_id):
         """Get album details."""
         # return await self._server.AudioLibrary.GetAlbumDetails(
         #     **_build_query(albumid=album_id, properties=properties)
         # )
         raise NotImplementedError
 
-    async def get_songs(self, artist_id=None, album_id=None, properties=None):
+    async def get_songs(self, artist_name=None, album_name=None):
         """Get songs list."""
-        # filter = {}
-        # if artist_id:
-        #     filter["artistid"] = artist_id
-        # if album_id:
-        #     filter["albumid"] = album_id
-        #
-        # return await self._server.AudioLibrary.GetSongs(
-        #     **_build_query(filter=filter, properties=properties)
-        # )
-        raise NotImplementedError
+        item_filter = AUDIO_FILTER
+        if artist_name:
+            item_filter = f'{item_filter} [Artist]=[{artist_name}'
+        if album_name:
+            item_filter = f'{item_filter} [Album]=[{album_name}'
+        ok, resp = await self._conn.get_as_list('Library/Values', params={'Field': 'Name', 'Files': item_filter})
+        return resp
 
-    async def get_movies(self, properties=None):
+    async def get_movies(self):
         """Get movies list."""
-        # return await self._server.VideoLibrary.GetMovies(
-        #     **_build_query(properties=properties)
-        # )
-        raise NotImplementedError
+        ok, resp = await self._conn.get_as_list('Library/Values', params={'Field': 'Name', 'Files': MOVIE_FILTER})
+        return resp
 
-    async def get_movie_details(self, movie_id, properties=None):
+    async def get_movie_details(self, movie_id: str):
         """Get movie details."""
         # return await self._server.VideoLibrary.GetMovieDetails(
         #     **_build_query(movieid=movie_id, properties=properties)
         # )
         raise NotImplementedError
 
-    async def get_seasons(self, tv_show_id, properties=None):
+    async def get_seasons(self, series: str):
         """Get seasons list."""
-        # return await self._server.VideoLibrary.GetSeasons(
-        #     **_build_query(tvshowid=tv_show_id, properties=properties)
-        # )
-        raise NotImplementedError
+        item_filter = TV_FILTER
+        if series:
+            item_filter = f'{item_filter} [Series]=[{series}]'
+        ok, resp = await self._conn.get_as_list('Library/Values',
+                                                params={'Field': 'Season', 'Files': item_filter})
+        return resp
 
-    async def get_season_details(self, season_id, properties=None):
+    async def get_season_details(self, season_id):
         """Get songs list."""
         # return await self._server.VideoLibrary.GetSeasonDetails(
         #     **_build_query(seasonid=season_id, properties=properties)
         # )
         raise NotImplementedError
 
-    async def get_episodes(self, tv_show_id, season_id, properties=None):
+    async def get_episodes(self, series: str, season: str) -> list:
         """Get episodes list."""
-        # return await self._server.VideoLibrary.GetEpisodes(
-        #     **_build_query(tvshowid=tv_show_id, season=season_id, properties=properties)
-        # )
-        raise NotImplementedError
+        item_filter = TV_FILTER
+        if series:
+            item_filter = f'{item_filter} [Series]=[{series}]'
+        if season:
+            item_filter = f'{item_filter} [Season]=[{season}]'
+        # TODO return episode field?
+        ok, resp = await self._conn.get_as_list('Library/Values',
+                                                params={'Field': 'Name', 'Files': item_filter})
+        return resp
 
-    async def get_tv_shows(self, properties=None):
+    async def get_tv_shows(self):
         """Get tv shows list."""
-        # return await self._server.VideoLibrary.GetTVShows(
-        #     **_build_query(properties=properties)
-        # )
-        raise NotImplementedError
+        ok, resp = await self._conn.get_as_list('Library/Values', params={'Field': 'Series', 'Files': TV_FILTER})
+        return resp
 
-    async def get_tv_show_details(self, tv_show_id=None, properties=None):
+    async def get_tv_show_details(self, tv_show_id=None):
         """Get songs list."""
         # return await self._server.VideoLibrary.GetTVShowDetails(
         #     **_build_query(tvshowid=tv_show_id, properties=properties)
