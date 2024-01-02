384d0c67d4207b5b52b4afef5e49b8feb65dd5ea
3ll3d00d <mattkhan@gmail.com>
2024-01-02 17:34:05 +0000

    expose new domain classes

--- hamcws/__init__.py
+++ hamcws/__init__.py
@@ -1,1 +1,12 @@
-from .hamcws import get_mcws_connection, MediaServer, MediaServerConnection, Zone, CannotConnectError, InvalidAuthError
+from .hamcws import (
+    get_mcws_connection,
+    MediaServer,
+    MediaServerConnection,
+    Zone,
+    PlaybackInfo,
+    PlaybackState,
+    MediaType,
+    MediaSubType,
+    CannotConnectError,
+    InvalidAuthError
+)
